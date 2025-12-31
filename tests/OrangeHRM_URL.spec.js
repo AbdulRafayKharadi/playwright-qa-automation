3509dc6ba44a055878237c313b0e07333ca61c5e
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-01-01 01:47:54 +0500

    Removed old basic test files after restructuring into 01-basics

--- tests/OrangeHRM_URL.spec.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-
-const{ test, expect} = require('@playwright/test')
-test('Validate OrangeHRM URL', async ({page}) => {
-    await page.goto('https://opensource-demo.orangehrmlive.com/');
-    await expect(page).toHaveURL(/.*orangehrmlive/);
-})
