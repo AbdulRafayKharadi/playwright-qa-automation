bef6ecd7cd9f09d554b957cde1bf73d7e7862a7c
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-01-01 14:30:09 +0500

    Replaced duplicate Title test with standardized filename

--- tests/01-basics/orangehrm-title.spec.js
+++ tests/01-basics/orangehrm-title.spec.js
@@ -4,3 +4,4 @@ test('Validate OrangeHRM Website title' , async({page}) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/');
     await expect(page).toHaveTitle(/OrangeHRM/);
 })
+
