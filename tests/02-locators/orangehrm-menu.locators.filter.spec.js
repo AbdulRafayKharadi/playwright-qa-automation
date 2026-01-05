dcdd27b736f90dc8f0883313b8c23efaf7db1836
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-01-06 00:03:03 +0500

    Added FILTER Locator test implementation

---
+++ tests/02-locators/orangehrm-menu.locators.filter.spec.js
@@ -0,0 +1,12 @@
+//@ts-check
+const { test, expect} = require('playwright/test')
+test('Validate MENU items by using FILTER Locator' , async ({page}) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+    await page.goto('https://opensource-demo.orangehrmlive.com/');
+    await page.getByPlaceholder('Username').fill('Admin');
+    await page.getByPlaceholder('Password').fill('admin123');
+    await page.getByRole('button' , {name: 'Login'}).click();
+    await page.waitForTimeout(10000);
+    // filter by text
+    await page.getByRole('listitem').filter({hasText: 'Leave'}).click();
+})
