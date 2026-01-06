f9789f45d02befcc18219a30cbc4869b5306d093
Abdul Rafay Kharadi <abdulrafaykharadi@gmail.com>
2026-01-06 23:21:02 +0500

    Fixed error in FILTER Locator test file

--- tests/02-locators/orangehrm-menu.locators.filter.spec.js
+++ tests/02-locators/orangehrm-menu.locators.filter.spec.js
@@ -2,11 +2,10 @@
 const { test, expect} = require('playwright/test')
 test('Validate MENU items by using FILTER Locator' , async ({page}) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/');
-    await page.goto('https://opensource-demo.orangehrmlive.com/');
     await page.getByPlaceholder('Username').fill('Admin');
     await page.getByPlaceholder('Password').fill('admin123');
     await page.getByRole('button' , {name: 'Login'}).click();
-    await page.waitForTimeout(10000);
     // filter by text
     await page.getByRole('listitem').filter({hasText: 'Leave'}).click();
+    await page.waitForTimeout(10000);
 })
